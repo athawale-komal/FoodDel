a0bd418ee0acade4c7637050b0b59d5b7618f5c1
pranita0426 <galandepranita7@gmail.com>
2025-12-27 15:05:21 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes  } from 'react-router-dom'
+import Header from './Components/Header'
+import Login from './Components/Login'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+    <Header/>
+  
+    <Routes>
+      <Route path='/login' element={<Login/>}></Route>
+    <Route path='/' element={<Home/>}></Route>
+    </Routes>
+  
+    </BrowserRouter>
+    <div>App
+    </div>
+    </>
   )
 }
 
